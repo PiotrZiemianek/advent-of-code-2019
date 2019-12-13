18e1967097e7f6f8c3e34fa04fc3abe570339ddc
Piotr Ziemianek <piotr.ziemianek@gmail.com>
2019-12-14 00:28:24 +0100

    day4 done

--- src/main/java/JvmProperties.java
+++ src/main/java/JvmProperties.java
@@ -6,5 +6,7 @@ public class JvmProperties {
         System.out.println(System.getProperty("java.vm.version"));
         System.out.println(System.getProperty("java.vm.vendor"));
         System.out.println(System.getProperty("java.vm.name"));
+
+        int[] ints = Integer.toString(6600).chars().map(c -> c - '0').toArray();
     }
 }
--- src/main/java/day4/Day4.java
+++ src/main/java/day4/Day4.java
@@ -1,4 +1,41 @@
 package day4;
 
+import java.util.*;
+
 public class Day4 {
+
+    public static void main(String[] args) {
+        int[] range = {168630, 718098};
+        List<Integer> maybePasswords = new ArrayList<>();
+        for (int password = range[0]; password <= range[1]; password++) {
+            int[] digits = Integer.toString(password).chars().map(c -> c - '0').toArray();
+            int beforDigit = 0;
+            boolean adjacentRule = false;
+            boolean neverDecrease = true;
+            Map<Integer, Integer> repeated = new HashMap<>();
+            for (int digit : digits) {
+                if (beforDigit == digit) {
+                    if (repeated.containsKey(digit)) {
+                        Integer counter = repeated.get(digit);
+                        counter++;
+                        repeated.put(digit, counter);
+                    } else {
+                        repeated.put(digit, 1);
+                    }
+                }
+                if (beforDigit > digit) {
+                    neverDecrease = false;
+                }
+                beforDigit = digit;
+            }
+            adjacentRule = repeated.containsValue(1);
+            if (adjacentRule && neverDecrease) {
+                maybePasswords.add(password);
+            }
+        }
+        for (Integer maybePassword : maybePasswords) {
+            System.out.println(maybePassword);
+        }
+        System.out.println(maybePasswords.size());
+    }
 }
