53bc288cd11ae25c1fd5addb9dcd09caf36815ed
Piotr Ziemianek <piotr.ziemianek@gmail.com>
2020-02-15 23:58:02 +0100

    day8 part2 done.

--- src/main/java/day8/Day8.java
+++ src/main/java/day8/Day8.java
@@ -4,6 +4,7 @@ import services.DataReaderFromFileService;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day8 {
@@ -25,7 +26,25 @@ public class Day8 {
         }
         int numberOfDigit1 = countNumberOfDigit('1', layers.get(indexFewest0layer));
         int numberOfDigit2 = countNumberOfDigit('2', layers.get(indexFewest0layer));
-        System.out.println(numberOfDigit1*numberOfDigit2);
+        System.out.println(numberOfDigit1 * numberOfDigit2);
+
+        System.out.println("-----------------------");
+        char[][] spaceImage = new char[6][25];
+        for (int y = 0; y < spaceImage.length; y++) {
+            for (int x = 0; x < spaceImage[y].length; x++) {
+                for (char[][] layer : layers) {
+                    char pixel = layer[y][x];
+                    if (pixel == '0') {
+                        spaceImage[y][x] = ' ';
+                        break;
+                    } else if (pixel == '1') {
+                        spaceImage[y][x] = 'X';
+                        break;
+                    }
+                }
+            }
+        }
+        printSpaceImage(spaceImage);
     }
 
     public static List<char[][]> getLayers(int width, int height, String spaceImageFormatPicture) {
@@ -55,5 +74,14 @@ public class Day8 {
             }
         }
         return counter;
+    }
+
+    public static void printSpaceImage(char[][] image) {
+        for (char[] digitsOnXAxis : image) {
+            for (char digit : digitsOnXAxis) {
+                System.out.print(digit+ " ");
+            }
+            System.out.println(); //next line on Y axis
+        }
     }
 }
