7b34694fbfaae1e8a4468149c50a4f10de7c0edd
Piotr Ziemianek <piotr.ziemianek@gmail.com>
2020-02-19 13:33:23 +0100

    day9 LongIntComputer class added.

---
+++ src/main/java/day9/Day9.java
@@ -0,0 +1,11 @@
+package day9;
+
+import java.nio.file.Path;
+
+public class Day9 {
+    static Path path = Path.of("src/main/resources/day9input.txt");
+
+    public static void main(String[] args) {
+
+    }
+}
